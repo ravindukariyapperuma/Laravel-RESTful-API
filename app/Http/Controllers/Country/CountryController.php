6c5f4c127caac5b32d35bffe62f97e32fa293e3a
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-12-13 16:24:34 +0530

    validation

--- app/Http/Controllers/Country/CountryController.php
+++ app/Http/Controllers/Country/CountryController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Models\CountryModel;
+use Validator;
 
 class CountryController extends Controller
 {
@@ -16,12 +17,21 @@ class CountryController extends Controller
     public function countryByID($id) {
         $country = CountryModel::find($id);
         if(is_null($country)){
-            return response()->json('Record not found!', 404);
+            return response()->json(["message" => "Record not found!"], 404);
         }
         return response()->json($country, 200);
     }
 
     public function countrySave(Request $request) {
+        $rules = [
+            'name' => 'required|min:3',
+            'iso' => 'required|min:2|max:2',
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if($validator->fails()){
+            return response()->json($validator->errors(), 400);
+        }
+
         $country = CountryModel::create($request->all());
         return response()->json($country, 201);
     }
@@ -29,7 +39,7 @@ class CountryController extends Controller
     public function countryUpdate(Request $request, $id){
         $country = CountryModel::find($id);
         if(is_null($country)){
-            return response()->json('Record not found!', 404);
+            return response()->json(["message" => "Record not found!"], 404);
         }
         $country->update($request->all());
         return response()->json($country, 200);
@@ -38,7 +48,7 @@ class CountryController extends Controller
     public function countryDelete(Request $request, $id){
         $country = CountryModel::find($id);
         if(is_null($country)){
-            return response()->json('Record not found!', 404);
+            return response()->json(["message" => "Record not found!"], 404);
         }
         $country->delete();
         return response()->json(null, 204);
