e7de6a172548d41a7d0e8676368805c00ed1e1a0
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-12-13 13:25:47 +0530

    record-not-found

--- app/Http/Controllers/Country/CountryController.php
+++ app/Http/Controllers/Country/CountryController.php
@@ -14,7 +14,11 @@ class CountryController extends Controller
     }
 
     public function countryByID($id) {
-        return response()->json(CountryModel::find($id), 200);
+        $country = CountryModel::find($id);
+        if(is_null($country)){
+            return response()->json('Record not found!', 404);
+        }
+        return response()->json($country, 200);
     }
 
     public function countrySave(Request $request) {
